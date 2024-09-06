c4172dc617956b80cd3bb0d30e0d61d8dea57222
prasannals <prasanna.lakkur@gmail.com>
2024-09-06 08:27:21 -0400

    Batch transcription

--- audio_transcription/ml/model.py
+++ audio_transcription/ml/model.py
@@ -12,3 +12,6 @@ class AudioTranscriptionModel:
     def transcribe(self, audio_path: str) -> str:
         self._validate_audio_path(audio_path)
         return self.model.transcribe(audio_path)["text"]
+    
+    def transcribe_batch(self, audio_paths: list[str]) -> list[str]:
+        return [{'file_path': audio_path, 'result': self.transcribe(audio_path)} for audio_path in audio_paths]
