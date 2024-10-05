8e01d9019717ecfaf41f9e98d81577dc2a6e790b
prasannals <prasanna.lakkur@gmail.com>
2024-10-05 18:35:40 -0400

    Fixed server code

--- audio_transcription/backend/server.py
+++ audio_transcription/backend/server.py
@@ -1,19 +1,19 @@
 from ..ml.model import AudioTranscriptionModel
 from flask_ml.flask_ml_server import MLServer
 from flask_ml.flask_ml_server.constants import DataTypes
-from flask_ml.flask_ml_server.models import FileInput, ImageResult, ResponseModel, TextInput, TextResult
+from flask_ml.flask_ml_server.models import BatchTextResult, TextInput, TextResult
 
 model = AudioTranscriptionModel()
 server = MLServer(__name__)
 
 @server.route('/transcribe', input_type=DataTypes.AUDIO)
-def transcribe(inputs: list[dict], parameters: dict):
+def transcribe(inputs: list[TextInput], parameters: dict) -> BatchTextResult:
     print('Inputs:', inputs)
     print('Parameters:', parameters)
-    files = [e['file_path'] for e in inputs]
+    files = [e.file_path for e in inputs]
     results = model.transcribe_batch(files)
-    results = [TextResult(text=e["file_path"], result=e["result"]) for e in results]
-    response = ResponseModel(results=results)
-    return response.get_response()
+    results = [TextResult(id=e["file_path"], result=e["result"]) for e in results]
+    results = BatchTextResult(results=results)
+    return results
 
 server.run()
